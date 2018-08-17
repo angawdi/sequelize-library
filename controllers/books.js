2bc3fda6c76569553cc012c54b2a6788336c44f9
Sky <anvandp18@gmail.com>
2018-08-17 15:16:37 -0700

    009

--- controllers/books.js
+++ controllers/books.js
@@ -7,7 +7,21 @@ var router = express.Router();
 
 // Define routes - remember they are relative to /books
 router.get('/', function(req, res){
-	res.send('stub for /books');
+	db.book.findAll().then(function(books){
+		console.log('Books found:', books);
+		res.render('books/index', {books: books});
+	}).catch(function(err){
+		console.log('oops', err);
+		res.render('404');
+	});
+});
+
+router.get('/new', function(req, res){
+	res.render('books/new');
+});
+
+router.post('/', function(req, res){
+	res.send(req.body);
 });
 
 // Export the router - this lets other files include me
